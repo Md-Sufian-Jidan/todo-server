8fbdec4575d7e6e9478a7e2fa53e801c12d8552d
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2025-03-26 21:07:00 +0600

    add todo api added

--- index.js
+++ index.js
@@ -33,13 +33,24 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        const todosCollection = client.db("todo-collection").collection("todos");
+
+        app.post('/todo', async (req, res) => {
+            const newTodo = req.body;
+            const result = await todosCollection.insertOne(newTodo);
+            res.send(result);
+        });
+
+
+
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
